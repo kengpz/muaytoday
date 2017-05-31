ad34ba91e34b95c0c4d75d8c87cefc526f5de660
kengpz <keng_pz@hotmail.com>
2017-06-01 01:33:47 +0700

    cm

---
+++ src/today/muay/program/dao/impl/muaydaoImpl.java
@@ -0,0 +1,7 @@
+package today.muay.program.dao.impl;
+
+import today.muay.program.dao.mauydao;
+
+public class muaydaoImpl implements mauydao {
+
+}
---
+++ src/today/muay/program/factory/ApplicationContextFactory.java
@@ -0,0 +1,25 @@
+package today.muay.program.factory;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ApplicationContextFactory {
+	private static ApplicationContextFactory applicationContextFactory;
+	private ApplicationContext context;
+
+	static {
+		applicationContextFactory = new ApplicationContextFactory();
+	}
+
+	private ApplicationContextFactory() {
+		context = new ClassPathXmlApplicationContext(new String[] { "\\resources\\mauy.xml" });
+	}
+
+	public static ApplicationContextFactory getInstance() {
+		return applicationContextFactory;
+	}
+
+	public Object getBean(String id) {
+		return context.getBean(id);
+	}
+}
